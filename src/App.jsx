2b1694a51aca80ba273d3ba10741eb446ccd692a
akram-abid <a.abid44778@gmail.com>
2025-03-25 23:18:47 +0100

    initilize the local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
-import MainBackground from "./components/MainBackground"
+import './App.css';
+import MainBackground from "./components/MainBackground";
 import IntroMain from './components/IntroMain';
 
 function App() {
-  console.log("retrive the try ", JSON.parse(localStorage.getItem("cart3")))
+  // Check if "cart3" exists in localStorage; if not, initialize it with an empty array
+  if (!localStorage.getItem("cart3")) {
+    localStorage.setItem("cart3", JSON.stringify([]));
+  }
+
+  console.log("retrieve the try ", JSON.parse(localStorage.getItem("cart3")));
+
   return (
     <>
-      <MainBackground url={"/videos/cyberpunk-skull-soldier-wallpaperwaifu-com.mp4"}/>
-      <IntroMain  />
+      <MainBackground url={"/videos/cyberpunk-skull-soldier-wallpaperwaifu-com.mp4"} />
+      <IntroMain />
     </>
   );
 }
 
-export default App
+export default App;
